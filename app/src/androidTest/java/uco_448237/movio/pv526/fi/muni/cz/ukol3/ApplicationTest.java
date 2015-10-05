0faf5d7faf07eef2801d1eca836e775fde52c525
blackmailcz <zbobo@seznam.cz>
2015-10-05 09:44:19 +0200

    Initial Commit - 03

---
+++ app/src/androidTest/java/uco_448237/movio/pv526/fi/muni/cz/ukol3/ApplicationTest.java
@@ -0,0 +1,13 @@
+package uco_448237.movio.pv526.fi.muni.cz.ukol3;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
